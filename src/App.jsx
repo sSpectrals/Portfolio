e909602f6285d951081c869de43cfbc4346d25b3
Edgar Mamikonian <e.mamikonian@student.tudelft.nl>
2025-11-11 22:36:28 +0100

    Deligation to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./Nav.jsx";
+import Hero from "./hero.jsx";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Nav />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
