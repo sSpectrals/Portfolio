c2a29cbba6955fc7d5555ca7ae4894a318b2e2df
Edgar Mamikonian <e.mamikonian@student.tudelft.nl>
2025-11-12 00:26:40 +0100

    Fixed hero size

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Nav from "./Nav.jsx";
-import Hero from "./hero.jsx";
+import Hero from "./Hero.jsx";
 
 const App = () => {
   return (
-    <main>
-      <Nav />
-      <Hero />
+    <main className="flex flex-col lg:flex-row min-h-screen">
+      <div className="w-full lg:w-1/2 flex-shrink-0">
+        <Hero />
+      </div>
+      <div className="w-full lg:w-1/2 flex-shrink-0">
+        <Nav />
+      </div>
     </main>
   );
 };
--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./hero.css";
 
 const Hero = () => {
-  return <section className="Page">hero</section>;
+  return (
+    <section
+      className="Page w-full h-fit ml-15 mt-15 mb-15"
+      style={{ width: "100%" }}
+    >
+      hero
+    </section>
+  );
 };
 
 export default Hero;
