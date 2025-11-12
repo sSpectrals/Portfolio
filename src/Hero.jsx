98698f887efeca443fc3702d910cff8567b311b5
Edgar Mamikonian <e.mamikonian@student.tudelft.nl>
2025-11-12 01:35:15 +0100

    Bug fix

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./hero.css";
 import City from "./components/City.jsx";
+import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
+import CanvasLoader from "./components/CanvasLoader";
 
-function Canvas() {
+function Scene() {
   return (
-    <canvas id="hero-canvas" className="w-full h-96">
-      <Suspense fallback={null}></Suspense>
-      <PerspectiveCamera makeDefault position={[0, 0, 3]} />
-      <City />
-    </canvas>
+    <Canvas id="hero-canvas" className="w-full h-screen">
+      <Suspense fallback={<CanvasLoader />}>
+        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={1} />
+        <City />
+      </Suspense>
+    </Canvas>
   );
 }
 
@@ -19,7 +24,7 @@ const Hero = () => {
       className="Page w-full h-fit ml-15 mt-15 mb-15"
       style={{ width: "100%" }}
     >
-      <Canvas />
+      <Scene />
     </section>
   );
 };
