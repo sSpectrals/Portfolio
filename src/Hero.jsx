b3fffc734ce5a66dbee08c0c8c29a66ec2b7adf1
Edgar Mamikonian <e.mamikonian@student.tudelft.nl>
2025-11-12 01:48:58 +0100

    fixed model scaling

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import "./hero.css";
-import City from "./components/City.jsx";
+import Panel from "./components/Panel.jsx";
 import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
 import CanvasLoader from "./components/CanvasLoader";
@@ -12,7 +12,7 @@ function Scene() {
         <PerspectiveCamera makeDefault position={[0, 0, 5]} />
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
-        <City />
+        <Panel scale={0.01} position={[0, -2, 0]} rotation={[0, 0, 0]} />
       </Suspense>
     </Canvas>
   );
@@ -22,9 +22,9 @@ const Hero = () => {
   return (
     <section
       className="Page ml-6 mt-6 mb-6 mr-6"
-      style={{ 
+      style={{
         height: "calc(100% - 3rem)",
-        width: "calc(100% - 3rem)"
+        width: "calc(100% - 3rem)",
       }}
     >
       <Scene />
