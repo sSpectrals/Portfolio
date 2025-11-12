ca7fc76a5a29a8e8d5a294b096c7078648b5a7e8
Edgar Mamikonian <e.mamikonian@student.tudelft.nl>
2025-11-12 02:05:49 +0100

    added leva

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,21 +4,51 @@ import Panel from "./components/Panel.jsx";
 import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
 import CanvasLoader from "./components/CanvasLoader";
+import { Leva, useControls } from "leva";
 
-function Scene() {
+function Scene({ controls }) {
   return (
-    <Canvas id="hero-canvas" className="w-full h-full">
-      <Suspense fallback={<CanvasLoader />}>
-        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
-        <Panel scale={0.01} position={[0, -2, 0]} rotation={[0, 0, 0]} />
-      </Suspense>
-    </Canvas>
+    <>
+      <Leva />
+      <Canvas id="hero-canvas" className="w-full h-full">
+        <Suspense fallback={<CanvasLoader />}>
+          <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Panel
+            rotation={[
+              controls.rotationX,
+              controls.rotationY,
+              controls.rotationZ,
+            ]}
+            position={[0, -2, 0]}
+            scale={0.01}
+          />
+        </Suspense>
+      </Canvas>
+    </>
   );
 }
 
 const Hero = () => {
+  const controls = useControls("Panel", {
+    rotationX: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationY: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+    rotationZ: {
+      value: 2.5,
+      min: -10,
+      max: 10,
+    },
+  });
+
   return (
     <section
       className="Page ml-6 mt-6 mb-6 mr-6"
@@ -27,7 +57,7 @@ const Hero = () => {
         width: "calc(100% - 3rem)",
       }}
     >
-      <Scene />
+      <Scene controls={controls} />
     </section>
   );
 };
