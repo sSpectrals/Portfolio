823039727b2bc64cece5b4743844de4f56f2233f
Edgar Mamikonian <e.mamikonian@student.tudelft.nl>
2025-11-17 00:12:53 +0100

    italic text

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -7,37 +7,39 @@ const AboutMe = () => {
       className="c-space bg-pastel-blue grid grid-cols-2 grid-rows-4 h-[200vh]"
     >
       {/* Row 1, Col 1 */}
-      <div className="text-center flex items-center justify-center min-h-[20vh] text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black col-start-1 row-start-1">
+      <div className="italic text-center flex items-center justify-center min-h-[20vh] text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black col-start-1 row-start-1">
         Hi, I'm Edgar Mamikonian
       </div>
       {/* Row 1, Col 2 */}
-      <div className=" text-center flex items-center justify-center col-start-2 row-start-1">
+      <div className=" min-h-[20vh] text-center flex items-center justify-center col-start-2 row-start-1">
         x
       </div>
       {/* Row 2, Col 1 */}
-      <div className=" text-center flex items-center justify-center col-start-1 row-start-2">
+      <div className=" min-h-[20vh] text-center flex items-center justify-center col-start-1 row-start-2">
         x
       </div>
       {/* Row 2, Col 2 */}
-      <div className=" w-full h-full text-center text-[clamp(2rem,2vw,6rem)] flex items-center justify-center col-start-2 row-start-2">
+      <div className="italic min-h-[20vh]  text-center text-[clamp(2rem,2vw,6rem)] flex items-center justify-center col-start-2 row-start-2">
         I am a computer science & engineering student with a passion for solving
         problems.
       </div>
       {/* Row 3, Col 1 */}
-      <div className="w-full h-full text-center text-[clamp(2rem,2vw,6rem)] flex items-center justify-center col-start-1 row-start-3">
+      <div className="italic min-h-[20vh] text-center text-[clamp(2rem,2vw,6rem)] flex items-center justify-center col-start-1 row-start-3">
         Currently, I'm expanding my skills in robotics & mathematics while
         applying everything in code.
       </div>
       {/* Row 3, Col 2 */}
-      <div className=" text-center flex items-center justify-center col-start-2 row-start-3"></div>
+      <div className=" min-h-[20vh] text-center flex items-center justify-center col-start-2 row-start-3">
+        x
+      </div>
       {/* Row 4, Col 1 */}
-      <div className=" text-center flex items-center justify-center col-start-1 row-start-4">
+      <div className=" min-h-[20vh] text-center flex items-center justify-center col-start-1 row-start-4">
         x
       </div>
       {/* Row 4, Col 2 */}
-      <div className=" w-full h-full text-center text-[clamp(2rem,2vw,6rem)] flex items-center justify-center col-start-2 row-start-4">
-        I'm always looking for new challenges and opportunities to grow as an
-        engineer.
+      <div className="italic min-h-[20vh] text-center text-[clamp(2rem,2vw,6rem)] flex items-center justify-center col-start-2 row-start-4">
+        I'm always looking for new challenges and opportunities to develop my
+        skillset.
       </div>
     </section>
   );
