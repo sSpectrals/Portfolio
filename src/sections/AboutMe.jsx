33964f1e64479d7e2f4e1a46ee0bcfa13ed7164f
Edgar Mamikonian <e.mamikonian@student.tudelft.nl>
2025-11-17 21:40:45 +0100

    better text placement

--- src/sections/AboutMe.jsx
+++ src/sections/AboutMe.jsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
         "Woo, I'm about to make a name for myself here" <br /> - Deadpool &
         Wolferine (2024)
       </div>
-      <div className="grid grid-cols-2 grid-rows-4 h-[200vh] pb-32">
+      <div className="grid grid-cols-2 grid-rows-4 gap-y-8 pb-36">
         {/* Row 1, Col 1 */}
         <div className=" rotate-3 italic text-center flex items-center justify-center min-h-[20vh] text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black col-start-1 row-start-1">
           <p className="bg-teal-400 border-r-4 border-r-pink border-b-4 border-b-pink p-5 ">
@@ -18,45 +18,33 @@ const AboutMe = () => {
           </p>
         </div>
         {/* Row 1, Col 2 */}
-        <div className=" min-h-[20vh] text-center flex items-center justify-center col-start-2 row-start-1">
+        <div className=" min-h-[20vh] text-center flex items-center justify-center col-start-2 row-start-1"></div>
+        {/* Row 2, Col 1 */}
+        <div className="col-start-1 row-start-2 flex items-center justify-center text-xl sm:text-3xl md:text-4xl lg:text-5xl italic text-center">
+          I am a computer science & engineering student with a passion for
+          solving problems.
+        </div>
+        {/* Row 2, Col 2: Image */}
+        <div className="col-start-2 row-start-2 flex items-center justify-center">
           <img
             className="w-32 h-32 sm:w-52 sm:h-52 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-80 xl:h-80 2xl:w-96 2xl:h-96 rounded-full"
             src={"assets/incognito.svg"}
             alt="me"
           />
         </div>
-        {/* Row 2, Col 1 */}
-        <div className="-rotate-12 min-h-[20vh] text-center flex items-center justify-center col-start-1 row-start-2">
-          <img
-            className="w-32 h-32 sm:w-52 sm:h-52 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-80 xl:h-80 2xl:w-96 2xl:h-96 "
-            src={"assets/student.svg"}
-            alt="student"
-          />
-        </div>
-        {/* Row 2, Col 2 */}
-        <div className="  italic min-h-[20vh] text-center text-1xl sm:text-3xl md:text-4xl lg:text-5xl flex items-center justify-center col-start-2 row-start-2">
-          I am a computer science & engineering student with a passion for
-          solving problems.
-        </div>
-        {/* Row 3, Col 1 */}
-        <div className="italic min-h-[20vh] text-center text-1xl sm:text-3xl md:text-4xl lg:text-5xl flex items-center justify-center col-start-1 row-start-3">
+        {/* Row 3, Col 1: Text */}
+        <div className="  col-start-1 row-start-3 flex items-center justify-center text-xl sm:text-3xl md:text-4xl lg:text-5xl italic text-center">
           Currently, I'm expanding my skills in robotics & mathematics while
           applying everything in code.
         </div>
         {/* Row 3, Col 2 */}
-        <div className="rotate-12 min-h-[20vh] text-center flex items-center justify-center col-start-2 row-start-3">
-          <img
-            className="w-32 h-32 sm:w-52 sm:h-52 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-80 xl:h-80 2xl:w-96 2xl:h-96 "
-            src={"assets/robot.svg"}
-            alt="robot"
-          />
-        </div>
+        <div className="rotate-12 min-h-[20vh] text-center flex items-center justify-center col-start-2 row-start-3"></div>
         {/* Row 4, Col 1 */}
         <div className=" min-h-[20vh] text-center flex items-center justify-center col-start-1 row-start-4">
           <img
             className="w-32 h-32 sm:w-52 sm:h-52 md:w-72 md:h-72 lg:w-80 lg:h-80 xl:w-80 xl:h-80 2xl:w-96 2xl:h-96 "
-            src={"assets/design.svg"}
-            alt="design"
+            src={"assets/robot.svg"}
+            alt="robot"
           />
         </div>
         {/* Row 4, Col 2 */}
